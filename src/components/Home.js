1f1b2ac822a9400779b1bac8ad066282a51a51a6
Manuela Gomez <mmanu.gomez@gmail.com>
2019-12-28 15:03:28 -0500

    [WIP] Home now reflects header content particular to construction website

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+import { Segment, Header } from 'semantic-ui-react';
 
 import Layout from './Layout';
 
 const Home = () => {
   return (
     <Layout>
-      <p>Hello World of React and Webpack! Hot reloaded :) </p>
-      <p>
-        <Link to="/dynamic">Navigate to Dynamic Page</Link>
-      </p>
+      <Segment vertical>
+        <Header as="h1">
+          We fix houses
+        </Header>
+        <p>Want us to fix your home? </p>
+        {/* <p>
+          <Link to="/dynamic">Navigate to Dynamic Page</Link>
+        </p> */}
+      </Segment>
     </Layout>
   );
 };
